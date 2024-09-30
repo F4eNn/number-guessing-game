474f86d6adf51370fd64945dc1b0612d035634ea
Mateusz <mateusz4k@outlook.com>
2024-09-30 20:23:35 +0200

    feat: allowed user play again, without restarting the game.

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 #!/usr/bin/env php
 <?php
 
-require_once __DIR__ . "/vendor/autoload.php";
+use App\Game;
+use App\SetupGame;
 
+require __DIR__ . "/vendor/autoload.php";
+
+$isInit = true;
+
+while (true) {
+    $setup = new SetupGame();
+    $level = $setup->setupGame($isInit);
+    $game = new Game($level);
+
+    $isInit = false;
+    $restart = readline("Do you want to play again? y/n:");
+
+    if ($restart !== "y") {
+        echo "Thank you for the game :)";
+        break;
+    }
+}
